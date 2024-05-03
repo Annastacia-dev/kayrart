d9230367436c7123d7e1c2633055b5b0e5637e1b
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-05-03 20:58:30 +0300

    display sketches

--- src/components/Sketches.jsx
+++ src/components/Sketches.jsx
@@ -1,10 +1,24 @@
+import useFetchArts from '../data/art';
+import Loader from './Loader';
+import Art from './Art';
+
 const Sketches = () => {
+  const { loading, sketches } = useFetchArts();
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
-    <div className="lg:px-20 lg:py-12 px-8 py-5 flex flex-col gap-5">
+    <div className="lg:px-20 lg:py-12 px-8 py-5 flex flex-col gap-10">
       <h1 className="font-ojuju font-bold capitalize text-3xl bg-primary w-40 px-5 py-1">
         sketches
       </h1>
       <p className="leading-7 lg:w-3/4">No colors, just amazing art.</p>
+      <div className="grid lg:grid-cols-4 grid-cols-2 gap-5">
+        {sketches.map((sketch) => {
+          return <Art key={sketch.id} art={sketch} />;
+        })}
+      </div>
     </div>
   );
 };
