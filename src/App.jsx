18a3cb6807feac40510ec403bd264dc0bb535a65
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-05-03 10:49:17 +0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-50 flex justify-center items-center">
+     <h1 className="font-ojuju font-bold capitalize text-4xl">kayrart</h1>
+    </div>
+  )
+}
+
+export default App
